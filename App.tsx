5f485de8374d280a14df59ec025410228c47efa7
Gawah Chan <gawahchan@gmail.com>
2019-08-24 17:57:02 +0100

    set up enzyme and created hello world test

--- App.tsx
+++ App.tsx
@@ -4,11 +4,18 @@ import { View, Text, StyleSheet } from 'react-native';
 class App extends React.Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Hello World!</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems:'center',
+    justifyContent:'center'
+  }
+})
 
 export default App;
--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,19 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App Component', () => {
+  it('renders correctly', () => {
+    renderer.create(<App />);
+  });
+  it('Display "Hello World"', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Text').contains('Hello World!')).toBe(true)
+  });
+})
